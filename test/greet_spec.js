d1a4cfacd46e175b69af2052364ceff551e03e25
glz <glz@mail.com>
2014-03-25 13:30:57 +0800

    the second lesson mocha-first-test

---
+++ test/greet_spec.js
@@ -0,0 +1,13 @@
+var greet = require('../index.js')
+var expect = require("chai").expect
+describe('greet', function(){
+  it("is a dummy success case", function(){
+    expect(1).to.eql(1);
+  });
+  it("should greet a person by name", function(){
+    expect(greet('Gaolz')).to.eq('hello, Gaolz');
+  });
+  it("should greet a person flirtatiously if drunk", function(){
+    expect(greet('Gaolz', true)).to.eq('hello Gaolz, what a sunny day!');
+  });
+});
