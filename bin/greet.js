4ec3c9127e2b900c4dafc9f3c60c69ed7e7adb1b
glz <glz@mail.com>
2014-03-25 11:04:45 +0800

    finish the first lesson

--- bin/greet.js
+++ bin/greet.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 var greet = require('../index.js');
-var result = greet((process.argv).slice(2));
-console.log(result);
+var parseArgs = require('minimist');
+var argv = parseArgs((process.argv).slice(2));
+console.log(greet(argv._[0], argv.drunk));
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // file: index.js
 
-module.exports = function greet(name){
-  return "hello, " + name;
+module.exports = function greet(name,drunk){
+  if(drunk) {
+    return "hello " + name + ", what a sunny day!";
+  }
+  else {
+    return "hello, " + name;
+  }
 }
